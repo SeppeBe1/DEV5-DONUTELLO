fbef9b89e05ab91d9ab21f3575901d470d28c3e7
Siebe <r0834360@student.thomasmore.be>
2022-11-30 16:18:39 +0100

    feat: add a test cube in threejs

--- Donutello/main.js
+++ Donutello/main.js
@@ -1,15 +1,65 @@
-//import latetst version three.js from unpkg
+//import threejs
 
-import * as THREE from 'https://unpkg.com/three@0.147.0/build/three.module.js';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
+import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
+
+//Scene
 const scene = new THREE.Scene();
-
+//Camera
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 5; // <- New code
+//Renderer
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setClearColor("#233143");
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const renderer = new THREE.WebGLRenderer();
-console.log(scene);
-console.log(camera);
-console.log(renderer);
+// Make Canvas Responsive
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+})
 
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+
+// Create Box
+const boxGeometry = new THREE.BoxGeometry(2, 2, 2);
+const boxMaterial = new THREE.MeshLambertMaterial({color: 0xFFFFFF});
+const boxMesh = new THREE.Mesh(boxGeometry, 
+boxMaterial);
+boxMesh.rotation.set(40, 0, 40);
+scene.add(boxMesh);
+
+//Lights
+const light = new THREE.PointLight(0xFFFFFF, 1, 100);
+light.position.set(5, 5, 5);
+scene.add(light);
+
+//orbit Controls for Camera 
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.screenSpacePanning = false;
+controls.minDistance = 1;
+controls.maxDistance = 5;
+
+
+camera.position.set( 0, 20, 100 );
+controls.update();
+
+
+
+// Rendering Function
+const rendering = function() {
+    requestAnimationFrame(rendering);
+
+    // Update trackball controls
+    controls.update();
+    // Constantly rotate box
+    scene.rotation.z -= 0.005;
+    scene.rotation.x -= 0.01;
+    renderer.render(scene, camera);
+}
+rendering();
