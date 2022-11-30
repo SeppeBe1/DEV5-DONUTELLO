6a0f7a4b286b1a68ad149f8990200139aea1fb95
Siebe <r0834360@student.thomasmore.be>
2022-11-30 13:13:09 +0100

    build: add threejs in main.js

--- Donutello/main.js
+++ Donutello/main.js
@@ -1,2 +1,15 @@
+//import latetst version three.js from unpkg
 
-setupCounter(document.querySelector('#counter'))
+import * as THREE from 'https://unpkg.com/three@0.147.0/build/three.module.js';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+console.log(scene);
+console.log(camera);
+console.log(renderer);
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
