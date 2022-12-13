2da95304a95bd5ef6b4c56927d75b5404d3991f2
unknown <seppe.bellens@hotmail.nl>
2022-12-13 12:13:23 +0100

    Feat[gallery]:all donuts appear in gallery

--- Donutello/main.js
+++ Donutello/main.js
@@ -1,2 +1,25 @@
+fetch ("https://donuttelloapi.onrender.com/api/v1/donuts", {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json"
+    }
+}).then(response => {
+    //console.log(response);
+    return response.json();
 
-setupCounter(document.querySelector('#counter'))
+})
+.then(json => {
+  for(let i = 0; i < json.data.donut.length ; i++ ){
+
+    let donut = `
+                  <div class="infoDonut" data-id="${json.data.donut[i]._id}">
+                    <img class="infoImg" src="../assets/donutUndrawTest.svg" alt="donut">
+                    <p>Bedrijf: <span>${json.data.donut[i].bedrijfsnaam}</span></p>
+                    <p>postcode: <span>${json.data.donut[i].postcode}</span></p>
+                    <a class="btnOrder" href="#">Bestel</a>
+                  </div>
+    `;
+    document.querySelector(".gallery").insertAdjacentHTML("afterbegin", donut);
+  }
+});
+
