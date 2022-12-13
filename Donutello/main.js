1f60e8c1f9de666410c60f935cb1b15df795569d
unknown <seppe.bellens@hotmail.nl>
2022-12-13 16:12:48 +0100

    Fix[main.js]: fix placement fetch api

--- Donutello/main.js
+++ Donutello/main.js
@@ -10,12 +10,15 @@ fetch ("https://donuttelloapi.onrender.com/api/v1/donuts", {
 })
 .then(json => {
   for(let i = 0; i < json.data.donut.length ; i++ ){
+    let datum = new Date(json.data.donut[i].datum);
+
 
     let donut = `
                   <div class="infoDonut" data-id="${json.data.donut[i]._id}">
                     <img class="infoImg" src="../assets/donutUndrawTest.svg" alt="donut">
                     <p>Bedrijf: <span>${json.data.donut[i].bedrijfsnaam}</span></p>
                     <p>postcode: <span>${json.data.donut[i].postcode}</span></p>
+                    <p>Datum: <span>${datum.toLocaleDateString()}</span></p>
                     <a class="btnOrder" href="#">Bestel</a>
                   </div>
     `;
