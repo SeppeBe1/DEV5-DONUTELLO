8f6d9ba1552067e353bf5cf06e738fe855a72a74
unknown <seppe.bellens@hotmail.nl>
2022-12-13 17:28:21 +0100

    Build[backend.js]: created donut component

--- Donutello/JavaScript/backend.js
+++ Donutello/JavaScript/backend.js
@@ -1,9 +1,35 @@
-// import Vue from 'vue';
+Vue.component('donut',{
+  template: `
+            <div>
+              <div class="donutData">
+              <img class="donut" :src="image" :alt="altDonut">
+              <span>Name: {{ name }}</span>
+              <span>Business: {{ business }}</span>
+              <span>Author: {{ author }}</span>
+              <span>Date: {{ date }}</span>
+              <span>Amount: {{ amount }}</span>
+            </div>
+              
+            <div class="ingredients">
+              <h2>Ingredients</h2>
+              <span>Topping: {{ topping }}</span>
+              <span>Glaze: {{ glaze }}</span>
+              <span>Dough: {{ dough }}</span>
+              <span>Filling: {{ filling }}</span> 
+            </div>
+
+            <div class="btn"></div>
+              <a href="">In productie</a>
+              <a href="">delete</a>
+            </div>
+            `,
+            props:["donut"]
+});
+
 var app = new Vue({
     el: '#app',
-      data: { 
-        author: 'Sepz',
-        donut: 'cranberry' 
+      data: {
+        donuts: [] 
       },
 
       mounted: function(){
@@ -12,7 +38,7 @@ var app = new Vue({
 
       methods:{
         getDonuts: function(){
-          console.log("kaas");
+          var that = this;
           fetch ("https://donuttelloapi.onrender.com/api/v1/donuts", {
                 method: "GET",
                 headers: {
@@ -23,8 +49,7 @@ var app = new Vue({
                 return response.json();
             })
             .then(json => {
-              console.log(json.data.donut);
-
+              that.donuts.push(json.data.donut);
             });
         }
 
